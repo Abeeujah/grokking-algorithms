3e14f3e978512e83d79ba064db78bd431f542e77
Abeeujah <abeeujah@gmail.com>
2024-08-21 20:24:42 +0100

    Grokking Algorithms - 1, 2

---
+++ src/binary_search.rs
@@ -0,0 +1,45 @@
+mod binary_search {
+    use std::cmp::Ordering;
+    pub fn binary_search<T>(list: &[T], item: &T) -> Option<usize>
+    where
+        T: Ord,
+    {
+        // Start searching from the middle
+        // If the item at the middle is
+        // the item you're looking for,
+        // return the middle index
+        // If the item at the middle is greater
+        // than the item you're searching for,
+        // Set search scope upper bound to be mid - 1
+        // Similarly, if the item at the middle is
+        // less than the item you're searching for,
+        // Set search scope lower bound to be mid + 1
+        let mut lower_bound = 0;
+        let mut upper_bound = list.len() - 1;
+
+        while lower_bound <= upper_bound {
+            let mid = (lower_bound + upper_bound) >> 1;
+            let guess = &list[mid];
+            match guess.cmp(&item) {
+                Ordering::Less => lower_bound = mid + 1,
+                Ordering::Greater => upper_bound = mid - 1,
+                Ordering::Equal => return Some(mid),
+            }
+        }
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::binary_search::*;
+
+    #[test]
+    fn test_binary_search() {
+        let list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        assert_eq!(Some(4), binary_search(&list[..], &5));
+        assert_eq!(None, binary_search(&list[..], &12));
+        assert_eq!(Some(0), binary_search(&list[..], &1));
+        assert_eq!(Some(9), binary_search(&list[..], &10));
+    }
+}
---
+++ src/selection_sort.rs
@@ -0,0 +1,49 @@
+mod selection_sort {
+    pub fn smallest<T: Ord + Copy>(list: &[T]) -> usize {
+        // Return the smallest Item in a list.
+        // Traverse the entire list to be sure.
+        let mut smallest_index = 0;
+
+        // Check the first item against every item in the list
+        // Update the smallest index anytime you encounter an item
+        // smaller than the previous one.
+        for (index, element) in list.iter().enumerate().skip(1) {
+            if element < &list[smallest_index] {
+                smallest_index = index;
+            }
+        }
+
+        // Return smallest index.
+        smallest_index
+    }
+
+    pub fn selection_sort<T: Ord + Copy>(list: Vec<T>) -> Vec<T> {
+        // Make a copy of the list.
+        // Copy will be mutated.
+        let mut sorted: Vec<T> = list;
+
+        // Swap for n -1 items in the list
+        // If Na and Nb are same, their positions won't change.
+        for i in 0..sorted.len() - 1 {
+            let smallest = smallest(&sorted[i..]);
+            sorted.swap(i, i + smallest);
+        }
+
+        // Return the sorted list.
+        sorted
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::selection_sort::*;
+    #[test]
+    fn test_selection_sort() {
+        assert_eq!(smallest(&vec![5, 3, 6, 2, 10]), 3);
+        assert_eq!(selection_sort(vec![5, 3, 6, 2, 10]), vec![2, 3, 5, 6, 10]);
+        assert_eq!(
+            selection_sort(vec![50, 3, 33, 87, 6, 2, 100]),
+            vec![2, 3, 6, 33, 50, 87, 100]
+        );
+    }
+}
